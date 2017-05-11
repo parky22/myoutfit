9c7465011821209ad83d9ae41142d839b6295065
Yoo-Nah Park <ypark2270@gmail.com>
2017-05-11 15:57:47 -0400

    create methods for handling input for form

--- client/components/AddClothingForm.js
+++ client/components/AddClothingForm.js
@@ -1,19 +1,79 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const AddClothingForm = props => {
+class AddClothingForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      type: 'Choose a type',
+      color: '',
+      image: '',
+      tags: ''
+    }
+  }
 
-  return (
-    <div className="row">
-      <h1> Add Clothing Form </h1>
-      <form className="col s12">
-        <div className="row">
-          <div className="input-field col s12">
-            <input placeholder="Clothing name" type="text" className="validate" />
+  handleChange(e) {
+    const target = e.target
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <h3> Add Clothing Form </h3>
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                name="name"
+                type="text"
+                className="validate"
+                value={this.state.name}
+                onChange={this.handleChange} />
+              <label>Clothing Name</label>
+            </div>
+            <div className="input-field col s12">
+              <select
+              name="type"
+              value={this.state.type}
+              onChange={this.handleChange}>
+                <option value="shirt">Shirt</option>
+                <option value="pants">Pants</option>
+                <option value="other">Other</option>
+              </select>
+              <label>Type of Clothing</label>
+            </div>
+            <div className="input-field col s12">
+              <input type="text" className="validate" />
+              <label>Colors</label>
+            </div>
+            <div className="input-field col s12">
+              <input type="text" className="validate" />
+              <label>Image</label>
+            </div>
+            <div className="input-field col s12">
+              <input type="text" className="validate" />
+              <label>Tags</label>
+            </div>
+            <button type="submit" onSubmit={this.handleSubmit}>
+              Submit
+          </button>
           </div>
-        </div>
-      </form>
-    </div>
-  );
-};
+        </form>
+      </div>
+    );
+  }
+}
 
-export default AddClothingForm;
+export default connect()(AddClothingForm);

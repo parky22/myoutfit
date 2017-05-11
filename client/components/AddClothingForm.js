9a2ef135c79b17d592ec7d3725dc52aa83a2adb5
Yoo-Nah Park <ypark2270@gmail.com>
2017-05-11 13:48:42 -0400

    include one form input for addClothing

--- client/components/AddClothingForm.js
+++ client/components/AddClothingForm.js
@@ -3,22 +3,17 @@ import React from 'react';
 const AddClothingForm = props => {
 
   return (
-    <div>
-      <h1> IN ADD CLOTHING FORM </h1>
+    <div className="row">
+      <h1> Add Clothing Form </h1>
+      <form className="col s12">
+        <div className="row">
+          <div className="input-field col s12">
+            <input placeholder="Clothing name" type="text" className="validate" />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
 
 export default AddClothingForm;
-
-
-//  <div className="row">
-//       <h1> Add Clothing Form </h1>
-//       <form className="col s12">
-//         <div className="row">
-//           <div className="input-field col s12">
-//             <input placeholder="Clothing name" type="text" className="validate" />
-//           </div>
-//         </div>
-//       </form>
-//     </div>

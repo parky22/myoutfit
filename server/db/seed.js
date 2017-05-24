fd9a6983ec3ce942e7456707d1892722c13449aa
Yoo-Nah Park <ypark2270@gmail.com>
2017-05-24 17:21:40 -0400

    add another shirt to seed

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,12 +1,5 @@
 const db = require('./index');
 
-const dummyPants = {
-  clothingType: 'pants',
-  color: ['green', 'multi'],
-  image: 'http://cdn.shopify.com/s/files/1/0286/5680/products/lunapants.png?v=1453829472',
-  tags: ['cute', 'floral', 'spring', 'casual']
-}
-
 const dummyShirt = {
   clothingType: 'shirt',
   color: ['purple'],
@@ -14,17 +7,34 @@ const dummyShirt = {
   tags: ['casual', 'light']
 };
 
-const dummyNoImage = {
+const dummyShirt2 = {
+  clothingType: 'shirt',
+  color: ['purple'],
+  image: 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=65090688',
+  tags: ['simple', 'frill']
+};
+
+const dummySweater = {
   clothingType: 'other',
   color: ['pink'],
   image: 'http://img.sheinside.com/images/sheinside.com/201310/1381733774262442938.jpg',
   tags: ['casual', 'warm']
 };
 
+const dummyPants = {
+  clothingType: 'pants',
+  color: ['green', 'multi'],
+  image: 'http://cdn.shopify.com/s/files/1/0286/5680/products/lunapants.png?v=1453829472',
+  tags: ['cute', 'floral', 'spring', 'casual']
+}
+
+
+
 const seedClothes = () => db.Promise.map([
-  dummyPants,
   dummyShirt,
-  dummyNoImage
+  dummySweater,
+  dummyShirt2,
+  dummyPants,
 ], clothing => db.model('clothing').create(clothing));
 
 db.sync({force: true})

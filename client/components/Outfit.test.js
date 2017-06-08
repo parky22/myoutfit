38a7c4f20786ef80e8ce12f4e6bfd12d4e1d9e94
Yoo-Nah Park <ypark2270@gmail.com>
2017-06-08 13:50:52 -0400

    add more tests for Outfit

--- client/components/Outfit.test.js
+++ client/components/Outfit.test.js
@@ -7,6 +7,7 @@ import Clothing from './Clothing';
 import { SmallHeader } from './style/headerStyle';
 
 const dummySweater = {
+  id: 1,
   clothingType: 'other',
   color: ['pink'],
   image: 'http://img.sheinside.com/images/sheinside.com/201310/1381733774262442938.jpg',
@@ -14,6 +15,7 @@ const dummySweater = {
 };
 
 const dummyPants = {
+  id: 2,
   clothingType: 'pants',
   color: ['green', 'multi'],
   image: 'http://cdn.shopify.com/s/files/1/0286/5680/products/lunapants.png?v=1453829472',
@@ -23,7 +25,7 @@ const dummyPants = {
 const dummyOutfit = [dummySweater, dummyPants];
 
 describe('<Outfit />', () => {
-  const wrapper = shallow(<Outfit outfit={dummyOutfit}/>);
+  const wrapper = shallow(<Outfit outfit={dummyOutfit} />);
 
   it('renders "outfit of the day" header', () => {
     expect(wrapper.contains(<SmallHeader>outfit of the day</SmallHeader>)).to.equal(true);
@@ -31,6 +33,14 @@ describe('<Outfit />', () => {
 
   it('renders 2 instances of Clothing component', () => {
     expect(wrapper.find(Clothing)).to.have.length(2);
+  });
+
+  it('renders a Clothing component for a sweater', () => {
+    expect(wrapper.contains(<Clothing key={dummySweater.id} piece={dummySweater} />)).to.equal(true);
+  });
+
+  it('renders a Clothing component for pants', () => {
+    expect(wrapper.contains(<Clothing key={dummyPants.id} piece={dummyPants} />)).to.equal(true);
   });
 
 });

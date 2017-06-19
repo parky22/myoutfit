b060c98a723002fc77b4b7c2c88cf49854e90a20
Yoo-Nah Park <ypark2270@gmail.com>
2017-06-19 10:31:05 -0400

    .setup.js is updated for jsdom v10~

--- .setup.js
+++ .setup.js
@@ -1,20 +1,18 @@
-require('babel-register')();
+const { JSDOM } = require('jsdom');
 
-const jsdom = require('jsdom').jsdom;
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
 
-const exposedProperties = ['window', 'navigator', 'document'];
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
 
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
-});
-
+global.window = window;
+global.document = window.document;
 global.navigator = {
-  userAgent: 'node.js'
+  userAgent: 'node.js',
 };
-
-documentRef = document;
+copyProps(window, global);
